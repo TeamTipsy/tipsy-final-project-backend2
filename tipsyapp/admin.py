bf0a69c55c87d550057ba6e1a695c9f84c4d956f
Bencodes1 <256.ben@gmail.com>
2021-04-19 16:54:50 -0400

    got models working, admin can add/edit

--- tipsyapp/admin.py
+++ tipsyapp/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .forms import MyUserCreationForm, MyUserChangeForm
 from .models import User, Venue
 
-admin.site.register(User, UserAdmin)
+class MyUserAdmin(UserAdmin):
+    add_form = MyUserCreationForm
+    form = MyUserChangeForm
+    model = User
+    list_display = [
+        'city',
+        'state', 
+        'bio_text',  
+        'prof_pic', 
+        'star_user',  
+        'users_following_num', 
+        'venues_following_num', 
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': (
+            'city',
+            'state', 
+            'bio_text',  
+            'prof_pic', 
+            'star_user',  
+            'users_following_num', 
+            'venues_following_num')}),
+    ) #this will allow to change these fields in admin module
+
+
+
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Venue)
---
+++ tipsyapp/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+class MyUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm):
+        model = User
+        fields = (
+            'city',
+            'state', 
+            'bio_text',  
+            'prof_pic', 
+            'star_user',  
+            'users_following_num', 
+            'venues_following_num' 
+            )
+
+class MyUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm):
+        model = User
+        fields = (
+            'city',
+            'state', 
+            'bio_text',  
+            'prof_pic', 
+            'star_user',  
+            'users_following_num', 
+            'venues_following_num'
+            )
