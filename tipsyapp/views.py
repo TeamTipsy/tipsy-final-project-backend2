8c00e6d12c5915cca4e33272f73d7fffb2c11b51
Bencodes1 <256.ben@gmail.com>
2021-04-21 19:24:06 -0400

    users can follow each other

--- tipsyapp/views.py
+++ tipsyapp/views.py
@@ -10,7 +10,21 @@ from .serializers import UserSerializer, VenueSerializer
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, 
+        IsOwnerOrReadOnly,
+        ]
+        
+    def put(self, request, pk):
+        obj = User.objects.get(user_id=pk)
+        if request.user not in obj.users_followed_by_list.all():
+            obj.users_followed_by_list.add(request.user)
+            request.user.users_following_list.add(obj.user_id)
+            return Response({'detail': 'User Followed'})
+        elif request.user in obj.users_followed_by_list.all():
+            obj.users_followed_by_list.remove(request.user)
+            request.user.users_following_list.remove(obj.user_id)
+            return Response({'detail': 'User Unfollowed'})    
 
 class UserList(generics.ListCreateAPIView):
     queryset= User.objects.all()
@@ -21,16 +35,17 @@ class UserList(generics.ListCreateAPIView):
 class VenueList(generics.ListCreateAPIView):
     queryset= Venue.objects.all()
     serializer_class = VenueSerializer    
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly, 
-        IsOwnerOrReadOnly,
-        ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
 
 
 class VenueDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Venue.objects.all()
     serializer_class = VenueSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, 
+        IsOwnerOrReadOnly,
+        ]
 
     def put(self, request, pk):
         obj = Venue.objects.get(venue_id=pk)
